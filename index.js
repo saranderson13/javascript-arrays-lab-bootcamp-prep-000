8f83e5a12910ac0092b023a8ee5dda914b0c67b5
Sarah Anderson <saranderson13@gmail.com>
2018-10-15 17:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  kittens = [...kittens, name];
-  return kittens;
+  moreKittens = [...kittens, name];
+  return moreKittens;
 }
