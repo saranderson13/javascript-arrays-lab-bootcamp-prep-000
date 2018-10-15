a79f5d75299e0006e889f024390e3add7d99d3aa
Sarah Anderson <saranderson13@gmail.com>
2018-10-15 17:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = [ "Milo", "Otis", "Garfield" ] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens = [...kittens, name];
+  return kittens;
+}
